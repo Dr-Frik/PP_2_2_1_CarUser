5d090074c5962a22094d1a8cca979e97166d2168
Nikita Frik <frik_nikita@mail.ru>
2023-08-13 01:51:56 +0300

    User have a car

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -10,15 +11,19 @@ import java.util.List;
 
 public class MainApp {
    public static void main(String[] args) throws SQLException {
-      AnnotationConfigApplicationContext context = 
-            new AnnotationConfigApplicationContext(AppConfig.class);
+      AnnotationConfigApplicationContext context =
+              new AnnotationConfigApplicationContext(AppConfig.class);
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      userService.add(new User("User1", "Lastname1", "user1@mail.ru",
+              new Car("BMW", 330)));
+      userService.add(new User("User2", "Lastname2", "user2@mail.ru",
+              new Car("AUDI", 6)));
+      userService.add(new User("User3", "Lastname3", "user3@mail.ru",
+              new Car("Porsche", 911)));
+      userService.add(new User("User4", "Lastname4", "user4@mail.ru",
+              new Car("BMW",540)));
 
       List<User> users = userService.listUsers();
       for (User user : users) {
@@ -28,6 +33,8 @@ public class MainApp {
          System.out.println("Email = "+user.getEmail());
          System.out.println();
       }
+      System.out.println(userService.getUserByModelSeries("BMW", 330).toString());
+      System.out.println(userService.getUserByModelSeries("BMW", 540).toString());
 
       context.close();
    }
